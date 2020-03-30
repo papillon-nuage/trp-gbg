9be15d613bedeb692c4ceb4416edfa045fad124f
papillon-nuage <62589143+papillon-nuage@users.noreply.github.com>
2020-03-30 18:37:14 +0200

    fix touch walk button attempt 7
    try without event

--- js/scripts.js
+++ js/scripts.js
@@ -378,12 +378,13 @@ function getWalkOnlyTripList(resultChosenWalkOnly) {
   .then(result => {
     console.log('WALK ONLY TRIPS'+JSON.stringify(result.TripList));
   });*/
+  window.location=walkString;
 }
 function walkButtonClicked(){
   console.log('TOUCHER' + walkString);
   window.location=walkString;
-  window.location.assign(walkString);
-  window.open(walkString);
+  //window.location.assign(walkString);
+  //window.open(walkString);
 
 }
 // geocoder.on('result', (resultChosen) =>{
@@ -397,9 +398,9 @@ function getPossibleTripList(resultChosen, newInputEntered){
   let walkHTML='';
   if(screen.width<=730) {
     walkHTML = '<div style="height: 40px;text-align: center;width: 30%;min-width: 150px;" class="singleTripBox" id="Walk999'+'">'
-    +'<span class="spanLeg" style="line-height: 40px; top: 0%; text-align: center;"><a id="bigWalkButton" style="color:black;text-decoration:none;"' // target="_blank" href="
-    //+'comgooglemaps://?saddr='+originLngLat.lat+','+originLngLat.lng+'&daddr='+resultChosen.result['center'][1]+','+resultChosen.result['center'][0]+'&directionsmode=walking'
-    +'>On y va à pied !</a></span></div>'; //"
+    +'<span class="spanLeg" style="line-height: 40px; top: 0%; text-align: center;"><a id="bigWalkButton" style="color:black;text-decoration:none;" target="_blank" href="' // target="_blank" href="
+    +'comgooglemaps://?saddr='+originLngLat.lat+','+originLngLat.lng+'&daddr='+resultChosen.result['center'][1]+','+resultChosen.result['center'][0]+'&directionsmode=walking'
+    +'">On y va à pied !</a></span></div>'; //"
     document.getElementById('possibleTripList').innerHTML+=walkHTML;
   }
   fetch("https://api.vasttrafik.se/bin/rest.exe/v2/trip?originCoordLat="+originLngLat.lat+"&originCoordLong="+originLngLat.lng+"&originCoordName="+originName
@@ -471,8 +472,8 @@ function getPossibleTripList(resultChosen, newInputEntered){
     //document.getElementById('possibleTripList').addEventListener('click', (event) => clickedResultTrip(event, result.TripList.Trip));
     document.getElementById('possibleTripList').removeEventListener('click', clickedResultTrip);
     document.getElementById('possibleTripList').addEventListener('click', clickedResultTrip);
-    document.getElementById('bigWalkButton').addEventListener('click', walkButtonClicked);
-    document.getElementById('bigWalkButton').addEventListener('touchstart', walkButtonClicked);
+    /*document.getElementById('bigWalkButton').addEventListener('click', walkButtonClicked);
+    document.getElementById('bigWalkButton').addEventListener('touchstart', walkButtonClicked);*/
   //}
   });
 
